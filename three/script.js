89557eed3e0bfae3ab0047510066f8629eedc3ea
Bateel Saber <sabeb234@newschool.edu>
2016-11-08 18:51:04 +0000

    test

--- three/script.js
+++ three/script.js
@@ -1,4 +1,7 @@
 
+ 
+ 
+ 
 $(".pinks").hover(function() {
 $(this).css("opacity", "0");
 },function(){
@@ -41,96 +44,3 @@ $(this).css("opacity", "0");
 },function(){
 $(this).css("opacity", "1");
 });
-
-// ================================
-
-
-
-// $(document).ready(function() {
-
-
-// $(document).keydown(function() {
-    
-//        var x =  Math.floor((Math.random()*26));
-//        var x2 =  Math.floor((Math.random()*26));
-       
-//        var w =  Math.floor((Math.random()*26));
-//        var w2 =  Math.floor((Math.random()*26));
-       
-//        var j =  Math.floor((Math.random()*26));
-//        var j2 =  Math.floor((Math.random()*26));
-       
-//        var z =  Math.floor((Math.random()*26));
-//        var z2 =  Math.floor((Math.random()*26));
-       
-//         var u =  Math.floor((Math.random()*26));
-//        var u2 =  Math.floor((Math.random()*26));
-       
-//         var m =  Math.floor((Math.random()*26));
-//        var m2 =  Math.floor((Math.random()*26));
-       
-//         var n =  Math.floor((Math.random()*26));
-//        var n2 =  Math.floor((Math.random()*26));
-       
-       
-//     var a = ".pink" + x;
-//     var a2 = ".pink" + x2;
-//     var b = ".yellow" + w;
-//     var b2 = ".yellow" + w2;
-//     var c = ".green" + j;
-//     var c2 = ".green" + j2;
-//       var d = ".purple" + z;
-//     var d2 = ".purple" + z2;
-//      var e = ".orange" + u;
-//     var e2 = ".orange" + u2;
-//          var f = ".blue" + m;
-//     var f2 = ".blue" + m2;
-//          var g = ".red" + n;
-//     var g2 = ".red" + n2;
-     
-    
-//     console.log("key");
-   
-//  $(a).css("opacity", "0"); 
-//  $(a2).css("opacity", "0"); 
-//  $(b).css("opacity", "0");
-//  $(b2).css("opacity", "0");
-//  $(c).css("opacity", "0");
-//  $(c2).css("opacity", "0");
-//  $(d).css("opacity", "0");
-//  $(d2).css("opacity", "0");
-//   $(e).css("opacity", "0");
-//  $(e2).css("opacity", "0");
-//   $(f).css("opacity", "0");
-//  $(f2).css("opacity", "0");
-//    $(g).css("opacity", "0");
-//  $(g2).css("opacity", "0");
- 
- 
- 
- 
-// });
-
-
-
-
-
-// $(document).keyup(function() {
-    
-
-
-// $(".pinks").css("opacity", "1");
-// $(".yellows").css("opacity", "1");
-// $(".greens").css("opacity", "1");
-// $(".purples").css("opacity", "1");
-// $(".oranges").css("opacity", "1");
-// $(".blues").css("opacity", "1");
-// $(".reds").css("opacity", "1");
-
-// });
-
-// console.log(a);
-
-
-
-// });
